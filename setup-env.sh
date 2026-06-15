5c2c76fc703b1f76686377b10cf1f6e0eb6a8229
B-OURGER <f.sudhoff@web.de>
2026-06-15 10:48:56 +0200

    fixed certificate creation for http-acme-eab / issued mode

--- setup-env.sh
+++ setup-env.sh
@@ -43,8 +43,8 @@ read -rp "Git organization name: " -e git_organization
 read -rp "Are you deploying from public git repositories? (Y/N): " -e git_public_repositories
 
 git_public_repositories=$([[ $git_public_repositories =~ ^[nN]([oO])?$ ]] && echo false || echo true)
-
-echo "Please enter the project base url, e.g. agri-gaia.example.com"
+# This has to change if any new subdomain is added which is longer than 13 characters. 
+echo "Please enter the project base url, e.g. agri-gaia.example.com. Note: It must not be longer than 50 characters."
 read -rp "Project base url: " -e project_base_url
 
 echo ""
